0402ad47bfa89018d3eae2ba5f9cd12683e12930
Carter Himmel <carter@elhnet.net>
2020-07-08 16:37:37 -0600

    chore: migrations

--- hasura/migrations/1594236418930_init/up.sql
+++ hasura/migrations/1594236418930_init/up.sql
@@ -1,18 +1,10 @@
 CREATE TABLE public.urls (
-    id bigint NOT NULL,
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
     short text NOT NULL,
     long text NOT NULL,
     created_at timestamp with time zone DEFAULT now() NOT NULL
 );
 COMMENT ON TABLE public.urls IS 'a shortened url';
-CREATE SEQUENCE public.urls_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-ALTER SEQUENCE public.urls_id_seq OWNED BY public.urls.id;
-ALTER TABLE ONLY public.urls ALTER COLUMN id SET DEFAULT nextval('public.urls_id_seq'::regclass);
 ALTER TABLE ONLY public.urls
     ADD CONSTRAINT urls_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.urls
